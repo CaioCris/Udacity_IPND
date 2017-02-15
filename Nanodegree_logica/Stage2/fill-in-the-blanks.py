40b41493e9fa073003debcdc825f9f9de6c8f444
CaioCris <caiohcris@gmail.com>
2017-02-14 23:54:08 -0200

    Project2 finished

--- Nanodegree_logica/Stage2/fill-in-the-blanks.py
+++ Nanodegree_logica/Stage2/fill-in-the-blanks.py
@@ -1,5 +1,5 @@
 # All the phrases with blanks
-very_easy = ("A common first thing to do in a language is display\n'Hello __1__!'  In __2__ this is particularly easy; all you have to do\nis type in:\n __3__ 'Hello __1__!'\nOf course, that isn't a very useful thing to do. However, it is an\nexample of how to output to the user using the __3__ command, and\nproduces a program which does something, so it is useful in that capacity.\n\nIt may seem a bit odd to do something in a Turing complete language that\ncan be done even more easily with an __4__ file in a browser, but it's\na step in learning __2__ syntax, and that's really its purpose.\n") #-world, python, print, 
+very_easy = ("A common first thing to do in a language is display\n'Hello __1__!'  In __2__ this is particularly easy; all you have to do\nis type in:\n __3__ 'Hello __1__!'\nOf course, that isn't a very useful thing to do. However, it is an\nexample of how to output to the user using the __3__ command, and\nproduces a program which does something, so it is useful in that capacity.\n\nIt may seem a bit odd to do something in a Turing complete language that\ncan be done even more easily with an __4__ file in a browser, but it's\na step in learning __2__ syntax, and that's really its purpose.\n") #-world, python, print, html
 easy = ("Almost all the __1__ languages provide a concept called __2__, which helps in executing one or more __3__ up to a desired number of times.\nAll high-level __1__ languages provide various forms of __2__, which can be used to execute one or more __3__ repeatedly.\nIn __4__ you have for __2__ and while __2__.\n") #-loop,-programming,-python,-statments
 medium = ("A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by\nadding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you\ndon't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,\ntuple, and ___4___ or can be more complicated such as objects and lambda functions.\n")#-function,-arguments,-None,-
 hard =  ("When you create a __1__, certain __2__s are automatically\ngenerated for you if you don't make them manually. These contain multipl\nunderscores before and after the word defining them.  When you writ\na __1__, you almost always include at least the __3__ __2__, definin\nvariables for when __4__s of the __1__ get made.  Additionally, you generall\nwant to create a __5__ __2__, which will allow a string representation\nof the method to be viewed by other developers.\n\nYou can also create binary operators, like __6__ and __7__, which\nallow + and - to be used by __4__s of the __1__.  Similarly, __8__,\n__9__, and __10__ allow __4__s of the __1__ to be compared\n(with <, >, and ==).\n")#-class,-method
@@ -8,10 +8,10 @@ hard =  ("When you create a __1__, certain __2__s are automatically\ngenerated f
 parts_in_blank  = ["__1__", "__2__", "__3__", "__4__", "__5__", "__6__", "__7__", "__8__", "__9__", "__10__"]
 
 # Awnsers of each list
-answer_very_easy = ["world", "Python", "print", "ok"]
-answer_easy = [ "Programming", "loops", "statments", "python"]
-answer_medium = ["function", "arguments", "None", "ok"]
-answer_hard = ["class", "method", "ok", "ok", "ok", "ok", "ok", "ok", "ok", "ok", "ok", "ok" ]
+answer_very_easy = ["world", "python", "print", "html"]
+answer_easy = [ "programming", "loops", "statments", "python"]
+answer_medium = ["function", "arguments", "None", "list"]
+answer_hard = ["class", "method", "__init__", "instance", "__repr__", "__add__", "__sub__", "__lt__", "__gt__", "__eq__"]
 
 #Functions
 #You can chose the level, and if the input is not value this will keep the while looping.
